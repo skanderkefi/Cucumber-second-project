bd04070bf3ba265bd904e293f7418fca384a7db4
skanderkefi <kefiskander99@gmail.com>
2021-02-24 10:39:45 +0100

    cucumber  commit

---
+++ src/main/java/cucumber/second/project/Calculator.java
@@ -0,0 +1,7 @@
+package cucumber.second.project;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/cucumber/second/project/testing/CucumberRunner.java
@@ -0,0 +1,10 @@
+package cucumber.second.project.testing;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+public class CucumberRunner {
+
+}
